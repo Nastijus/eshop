e209d791f57ef587239019f91e9b0d8c1bab4300
Justinas Rupeika <nastijus@gmail.com>
2025-05-07 17:08:09 +0300

    Search, product page and authentication have been added

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from shop import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('shop', views.shop, name='shop')
+    path('shop', views.shop, name='shop'),
+    path('search', views.search, name='search'),
+    path('product/<int:product_id>', views.product, name='product'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,27 +1,39 @@
+from itertools import product
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from shop.models import Category, Product
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
     categories = Category.objects.all()
-    # products = Product.objects.all()
+    return render(request, 'index.html', {'categories': categories})
 
-
-    context = {
-        'categories':categories,
-        # 'products': products,
-      }
-
-    return render(request, 'index.html', context=context)
 
 def shop(request):
     categories = Category.objects.all()
-    products = Product.objects.all()
-
+    query = request.GET.get('query')
+    if query:
+        products = Product.objects.filter(Q(product_name__icontains=query))
+    else:
+        products = Product.objects.all()
 
     context = {
-        'categories':categories,
+        'categories': categories,
         'products': products,
-      }
-
+        'query': query,
+    }
     return render(request, 'shop.html', context=context)
+
+
+def search(request):
+    query = request.GET.get('query')
+    return HttpResponseRedirect(f"{reverse('shop')}?query={query}")
+
+
+def product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'product.html', {'product': product})
